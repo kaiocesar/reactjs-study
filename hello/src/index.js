fad2ea167dfd5c928bacc3dbc1a65bc028d845ec
Kaio Cesar <tecnico.kaio@gmail.com>
2017-12-11 16:01:11 -0200

    ajustes para o translate

--- hello/src/index.js
+++ hello/src/index.js
@@ -7,10 +7,12 @@ import registerServiceWorker from './registerServiceWorker';
 import HelloWorld from './HelloWorld'
 import PropsValidation from './PropsValidation'
 import Events from './Events'
+import Translate from './Translate'
 
 ReactDOM.render(
     // <PropsValidation name="Bruna" age="27" height="160" />, 
-    <Events />,
+    // <Events />,
+    <Translate />,
     document.getElementById('root')
 );
 
